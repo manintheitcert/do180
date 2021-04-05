7a12c88222217eee807dad71cc955dfc96a9afd6
manintheitcert <manintheitcert@outlook.com>
2021-04-05 17:22:35 +0200

    update: entry-point-postgre.sh

--- postgres/entry-point-postgre.sh
+++ postgres/entry-point-postgre.sh
@@ -3,9 +3,12 @@ set -m
 
 echo "Setting up Postgresql Database..."
 
+cnt=$(ls /var/lib/pgsql/data | wc -l)
+echo ""
+echo ""
+
+if [ "${cnt}" -eq "0" ]; then
 initdb -D /var/lib/pgsql/data
-
-
 cat <<-EOF > /var/lib/pgsql/data/pg_hba.conf 
 host  all  all 0.0.0.0/0 md5
 local all  all trust
@@ -14,11 +17,13 @@ EOF
 cat <<-EOF > /var/lib/pgsql/data/postgresql.conf
 listen_addresses = '*'
 EOF
-
+fi
 
 /usr/bin/postgres -D /var/lib/pgsql/data -c  config_file=/var/lib/pgsql/data/postgresql.conf &
 
 sleep 10
+
+if [ "${cnt}" -eq "0" ]; then
 
 if [ ! -z "${POSTGRE_USER}" ] && [ ! -z "${POSTGRE_PASSWORD}" ] && [ ! -z "${POSTGRE_DATABASE}" ]; then
   psql --command "CREATE USER ${POSTGRE_USER} WITH SUPERUSER PASSWORD '${POSTGRE_PASSWORD}';"
@@ -31,6 +36,7 @@ if [ ! -z "${POSTGRE_ROOT_PASSWORD}" ]; then
   psql --command "ALTER USER postgres WITH PASSWORD '${POSTGRES_ROOT_PASSWORD}';"
   else
     echo "ROOT IS NOT SET"
+fi
 fi
 
 fg %1 
